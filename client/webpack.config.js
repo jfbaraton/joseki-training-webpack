95177cd8f33cb66b8c89863462fb5100bf9add77
jeffbaraton <jean-francois.baraton@elisa.fi>
2022-12-11 11:31:13 +0200

    init webpack for client and server. client done, server WIP

---
+++ client/webpack.config.js
@@ -0,0 +1,51 @@
+// webpack.config.js //
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require('path')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: { loader: "babel-loader" }
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: { loader: "babel-loader" }
+            },
+            {
+                test:  /\.html$/,
+                use: { loader: "html-loader" }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpg|gif|jpeg|svg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: './resources/imgs',
+                            name: '[name].[ext]'
+                        }
+                    },
+                ]
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./public/index.html",
+            filename: "./index.html"
+        })
+    ],
+}
